cec534234282b607f46ed0367d0bc54bf43c71bf
mentalhealthawhereness <38903439+mentalhealthawhereness@users.noreply.github.com>
2021-01-14 11:57:59 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -91,22 +91,10 @@ window.onload = function () {
         position: 'topleft'}); //Adds Search toolbar; OSMGeocoder
     mymap.addControl(osmGeocoder);
     
-    var leafletView = new PruneClusterForLeaflet();
-    for (var i = 0, l = addressPoints.length; i < l; ++i) {
-        leafletView.RegisterMarker(new PruneCluster.Marker(addressPoints[i][0], addressPoints[i][1], {title: addressPoints[i][2]}));
-    }
-    for (i = 0, l = addressPoints2.length; i < l; ++i) {
-        leafletView.RegisterMarker(new PruneCluster.Marker(addressPoints2[i][0], addressPoints2[i][1], {title: addressPoints[i][2]}));
-    }
-
-    leafletView.PrepareLeafletMarker = function (marker, data) {
-        if (marker.getPopup()) {
-            marker.setPopupContent(data.title);
-        } else {
-            marker.bindPopup(data.title);
-        }
-    };
-    mymap.addLayer(leafletView);
+    var pruneCluster = new PruneClusterForLeaflet();
+    var marker = new PruneCluster.Marker(places);
+    pruneCluster.RegisterMarker(marker);
+    }).addTo(mymap);
     
     
     //// load dataset from json ////////
